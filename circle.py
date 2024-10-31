140465dd799e5e873ec054ef608a421524c46d4c
SokovMM <misha.sokovih@gmail.com>
2024-10-31 21:56:13 +0300

    added doc

--- circle.py
+++ circle.py
@@ -1,10 +1,13 @@
 import math
 
 
-def area(r):
+def AreaCircle(r):
+    '''принимает r и возвращает квадрат r умноженный на значение хранящиеся в math.pi'''
+    '''Пример вызова: AreaCircle(1) - вернет math.pi * 1 * 1. '''
     return math.pi * r * r
 
 
-def perimeter(r):
+def PerimeterCircle(r):
+    '''принимает r и возвращает r умноженное на значение хранящиеся в math.pi и умноженное на 2'''
+    '''Пример вызова: PerimeterCircle(1) - вернет 2 * math.pi * 1. '''
     return 2 * math.pi * r
-
